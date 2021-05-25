c03e3b6c390717fcd1fd9754b393d31f46fcf2bd
mouaiadsalaas <mouaiadsalaas@gmail.com>
2021-05-25 13:11:19 +0300

    first commit

---
+++ Debug/source/can.d
@@ -0,0 +1,35 @@
+# FIXED
+
+source/can.obj: ../source/can.c
+source/can.obj: C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/can.h
+source/can.obj: C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/reg_can.h
+source/can.obj: C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/sys_common.h
+source/can.obj: C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/hal_stdtypes.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+source/can.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h
+source/can.obj: C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/sys_vim.h
+source/can.obj: C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/reg_vim.h
+
+../source/can.c: 
+C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/can.h: 
+C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/reg_can.h: 
+C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/sys_common.h: 
+C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/hal_stdtypes.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h: 
+C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/sys_vim.h: 
+C:/Users/inovar/workspace_v9/TMS570LS07_CANBUS/include/reg_vim.h: 
